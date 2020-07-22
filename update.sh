66fe4801783c794dbce6169c9616b6176bf5585c
mirco <mircomannino1997@gmail.com>
2020-07-22 14:46:32 +0200

    Fix update script

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Remove the git tracking
-rm -rf ${PWD}/.git* || rm -rf ${PWD}/.git*
-
+rm -rf ${PWD}/.git* || rm -rf ${PWD}/Ricevute/.git*
 
 # Get the repository -> "Ricevute" directory
 git clone https://github.com/mircomannino/Ricevute.git
 
 # Get file from directory and delete it
+rm -r __pychache__
 mv ${PWD}/Ricevute/* ./
 rm -rf Ricevute
